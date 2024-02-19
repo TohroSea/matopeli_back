9938c42f86719dc7443c830a0194511aac82ca69
Hroulev <Thomas.Hroulev@seamk.fi>
2024-02-19 13:44:32 +0200

    added 2 endpoints

--- app/api.py
+++ app/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, abort
 from models import db, Score
+import json
 
 
 api = Blueprint("api", __name__)
@@ -27,5 +28,15 @@ def submit():
 
 @api.route("/highscores", methods=["GET"])
 def highscores():
-	if not request.json:
-		abort(401)
+	results = Score.query.order_by(Score.points.desc()).limit(10).all()
+	data = []
+	for result in results:
+		data.append({"name": result.name, "points": result.points})
+	return json.dumps(data, ensure_ascii=False, indent="\t")
+
+
+@api.route("/clear", methods=["GET"])
+def clear():
+	Score.query.delete()
+	db.session.commit()
+	return "ok"
