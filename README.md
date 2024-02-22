5210fdd66682f0103f160cd037f70794b694ae9d
Hroulev <Thomas.Hroulev@seamk.fi>
2024-02-22 10:47:17 +0200

    added cors support

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# matopelikuosi
+# Matopelin Azure backend
+joo
--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+
 from models import db
 from api import api
 
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object("config")
 db.init_app(app)
 with app.app_context():
